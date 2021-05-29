c7346d3b1ee54d512b67b74d0b88b5f4e986eb1e
Hon <dennis.mejen@gmail.com>
2021-05-29 17:20:07 +0700

    [Added] Creating the Modal

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import * as React from "react";
 import Title from "./comps/Title";
 import UploadForm from "./comps/UploadForm";
 import ImageGrid from "./comps/ImageGrid";
+import Modal from "./comps/Modal";
 
 function App() {
+  const [selectedImg, setSelectedImg] = React.useState(null);
+
   return (
     <div className="App">
       <Title />
       <UploadForm />
-      <ImageGrid />
+      <ImageGrid setSelectedImg={setSelectedImg} />
+      {selectedImg && (
+        <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} />
+      )}
     </div>
   );
 }
