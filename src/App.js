b9bb69efff88bc66b99fe5daf688efebc6de9d12
Hon <dennis.mejen@gmail.com>
2021-05-29 12:05:53 +0700

    [Added] Firestore Hook & Showing Images

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import * as React from "react";
 import Title from "./comps/Title";
 import UploadForm from "./comps/UploadForm";
+import ImageGrid from "./comps/ImageGrid";
 
 function App() {
   return (
     <div className="App">
       <Title />
       <UploadForm />
+      <ImageGrid />
     </div>
   );
 }
